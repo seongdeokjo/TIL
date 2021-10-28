c5e18707c1b4dde9d05529901dccf123800101ca
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-10-28 14:13:05 +0900

    feat: dp 문제

---
+++ C,C++/알고리즘/DP_baekjoon_11726.cpp
@@ -0,0 +1,27 @@
+/* 
+	다이나믹 프로그래밍 타일링 문제  
+	백준 11726 : 2xn 타일링 
+*/
+
+#include <stdio.h>
+
+int d[1001];
+
+int dp(int x){
+	if(x == 1){
+		return 1;
+	}
+	if(x == 2){
+		return 2;
+	}
+	if(d[x] != 0){
+		return d[x];
+	}
+	return d[x] = (dp(x - 1) + dp(x - 2)) % 10007; 
+}
+
+int main(void){
+	int x;
+	scanf("%d", &x);
+	printf("%d", dp(x));
+}

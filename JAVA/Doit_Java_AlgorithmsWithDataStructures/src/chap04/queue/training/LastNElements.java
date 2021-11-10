ac5e25292e3ececb10c791935158491626216ad2
seongdeokjo <79972900+seongdeokjo@users.noreply.github.com>
2021-11-10 17:34:26 +0900

    feat: make queue structure example

---
+++ JAVA/Doit_Java_AlgorithmsWithDataStructures/src/chap04/queue/training/LastNElements.java
@@ -0,0 +1,36 @@
+package chap04.queue.training;
+
+import java.util.Scanner;
+
+public class LastNElements {
+	//원하는 개수만큼 값을 입력 받아 요솟수가 N인 배열에 마지막 N개를 저장
+	
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		final int N = 10;
+		int[] a = new int[N];
+		int cnt = 0;
+		int retry;
+		
+		System.out.println("정수를 입력하세요.");
+		
+		do {
+			System.out.printf("%d번째 정수 :",cnt + 1);
+			a[cnt++ % N] = stdIn.nextInt();
+			
+			System.out.print("계속 할까요? 1.예 / 2.아니오 : ");
+			retry = stdIn.nextInt();
+		}while(retry == 1);
+		
+		int i = cnt - N;
+		if(i < 0) {
+			i = 0;
+		}
+		
+		for(; i < cnt; i++) {
+			System.out.printf("%2d번째의 정수 =%d\n",i + 1,a[i % N]);
+		}
+		
+		
+	}
+}
